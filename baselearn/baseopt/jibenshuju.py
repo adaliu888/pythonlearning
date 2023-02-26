074f5dfb6db095afc6eb79575574b14886f880de
Martin <123860165+adaliu888@users.noreply.github.com>
2023-02-26 21:46:04 +0800

    master
    baselearn\baseopt\jibenshuju.py

--- baselearn/baseopt/jibenshuju.py
+++ baselearn/baseopt/jibenshuju.py
@@ -44,4 +44,123 @@ print(isinstance(B(), A))
 print(type(B()) == A)
 
 
-issubclass(bool, int)
+print(issubclass(bool, int))
+
+a = [1, 2, 3, 4, 5]
+print(a[0])
+print(a[2:5])
+print(a*2)
+print(a[2:])
+
+
+def reverseword(input):
+    inputWords = input.split(' ')
+    print(inputWords)
+
+    # 翻转字符串
+    inputwords = inputWords[-1:]
+    print(inputwords)
+    inputWords = inputWords[:-1]
+
+    print(inputWords)
+    output = ' '.join(inputWords)
+    return output
+
+
+if __name__ == '__main__':
+    input = 'I like python'
+    rw = reverseword(input)
+    print(rw)
+# tuple0(元组)
+tuple0 = ('abcd', 'runoob', 'google', 123, 789, 70.2)
+tinytuple0 = (123, 'runoob')
+
+print(tuple0)
+
+print(tuple0[0])
+
+for x in tuple0:
+    print(x)
+
+print(tuple0 * 2)
+
+print(tuple0 + tinytuple0)
+print(tuple0[1:2])
+
+
+# 集合，类型一样的，长度可以变
+
+sites = {}
+print(sites)
+sites = set()
+print(sites)
+sites = {'abd', 123}
+print(sites)
+if "gogle" in sites:
+    print("gogle in sites")
+else:
+    print("gogle not in sites")
+
+
+# 集合运算
+
+a = set("abcdefsddr")
+b = set('alarcazam')
+print(a)
+print(b)
+print(a-b)  # 差集
+print(b-a)
+print(a | b)  # 并集
+print(a & b)  # 交集
+print(a ^ b)  # a和b中不同时存在的元素
+
+
+# maping
+
+dict = {}
+dict['one'] = "1-who?"
+dict[2] = "2-am"
+print(dict)
+tinydict = {"name": "runoob", "code": 1, "sit": "www.runoob.com"}
+print(dict["one"])
+print(dict[2])
+print(tinydict.keys())
+print(tinydict.values())
+print(tinydict.items())
+
+
+for x, y in tinydict.items():
+
+    print(x, y)
+
+
+a = int('12', 16)
+print(a)
+a = int('0xa', 16)
+print(a)
+a = int('10', 8)
+print(a)
+
+
+float(1)
+print(float(1))
+print(float('123'))
+x = 'abc'
+print(repr(x))
+
+
+x = 7
+print(eval('3*7'))
+
+
+def f():
+    list1 = ['runoob', 'google', 'baidu', 'sina']
+    tuple01 = tuple(list1)
+    print(tuple01)
+
+
+if __name__ == '__main__':
+    input = 'I like python'
+    rw = reverseword(input)
+    print(rw)
+    f()
