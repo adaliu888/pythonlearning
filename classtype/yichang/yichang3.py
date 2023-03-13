1573b1bfebb38c49baf29d1918e3ce766a8dbec4
Martin <123860165+adaliu888@users.noreply.github.com>
2023-03-13 21:25:42 +0800

    master
    yichang3.py
    Nstr.py
    num.py

--- classtype/yichang/yichang3.py
+++ classtype/yichang/yichang3.py
@@ -1,1 +1,9 @@
 print(type(zip()))
+print("\n")
+
+for line in open("myfile.txt"):
+    print(line, end="")
+print("\n")
+with open("myfile.txt") as f:
+    for line in f:
+        print(line, end="")
